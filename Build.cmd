9ed4b7a941c252cd79c51b9c21e0acf505faddfb
Licshee <licshee@yen.ai>
2016-04-06 18:27:49 +0800

    remove unnecessary "noinlining"s, add "aggressiveinlining"

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 call Build.Net45.cmd
-powershell -ep - "sc 'ILTricks.NoAggressiveInlining.il' (gc 'ILTricks.il').Replace('aggressiveinlining', '')"
+powershell -ep - "sc 'ILTricks.NoAggressiveInlining.il' (gc 'ILTricks.il').Replace(' aggressiveinlining', '')"
 call Build.Portable.cmd
 call Build.Net35.cmd
 powershell -ep - "sc 'ILTricks.NoLinq.il' (gc 'ILTricks.NoAggressiveInlining.il').Replace('[System.Core]', '')"
